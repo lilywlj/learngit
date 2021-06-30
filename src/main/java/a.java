cb23f82f439671b86d024b1a839d6c57b72e0d4e
lilywlj <2232577286@qq.com>
2021-06-30 10:49:33 +0800

    first commit

---
+++ src/main/java/a.java
@@ -0,0 +1,11 @@
+/**
+ * @author Salong
+ * @description
+ * @date 2021/6/30 10:48
+ * @Email:salong0503@aliyun.com
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("this is a git test!!!!");
+    }
+}
